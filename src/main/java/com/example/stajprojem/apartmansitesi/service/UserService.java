82c9d3756f68b1e255e8ea3c147e755a7959b1f6
mervecabuk <mervecabbuk@gmail.com>
2024-08-23 09:25:48 +0300

    user service update

--- src/main/java/com/example/stajprojem/apartmansitesi/service/UserService.java
+++ src/main/java/com/example/stajprojem/apartmansitesi/service/UserService.java
@@ -10,6 +10,8 @@ public class UserService {
         // Burada kullanıcı doğrulama işlemini yapacağız.
         // Örneğin, kullanıcı adı ve şifreyi veritabanında kontrol edebiliriz.
         // Şimdilik basit bir kontrol ekleyelim.
+
+        String user = "user";
         return "testuser".equals(username) && "testpassword".equals(password);
     }
 }
